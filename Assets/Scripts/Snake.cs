7795c632d3ffbb05954f539892ec98e46f232f2f
Josie Thompson <josiahst@gmail.com>
2019-06-11 21:30:17 -0700

    Wrote the specifications for the snake class

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** Snake
+ * Provide information and control about the snake's movement
+ *
+ * Constructor Summary
+ *
+ *  public Snake() - Create a new snake.
+ *
+ * Method Summary
+ *
+ *  public void Move(Vector3 dir)
+ *      Move the snake one unit in the direction of dir.
+ *
+ *  public bool Eats(Vector3 pos)
+ *      Determine if the snake has eaten the object at the given position.
+ *      
+ *  public bool Contains(Vector3 pos)
+ *      Determine if the snake's body contains the given position.
+ */
+public class Snake
+{
+    /** public Snake()
+     * Create a new snake.
+     *
+     * New snakes have bodies 4 units long.
+     */
+    public Snake() {
+    }
+
+    /** public void Move(Vector3 dir)
+     * Move the snake one unit in the direction of dir.
+     *
+     * Parameters:
+     *  dir - direction to move in. 3rd dimension is ignored.
+     *
+     * Preconditions
+     *  dir is within the set {left, right, up, down}
+     *
+     * Postconditions:
+     *  All parts of the snake are continuous and have not made any sudden
+     *  jumps in position.
+     */
+    public void Move(Vector3 dir) {
+        // TODO: Implement
+    }
+
+    /** public bool Eats(Vector3 pos)
+     * Determine if the snake has eaten the object at the given position.
+     *
+     * Parameters:
+     *  pos - position to compare
+     *
+     * Postconditions:
+     *  Eats will only compare the given position with the head of the snake.
+     */
+    public bool Eats(Vector3 pos) {
+        // TODO: Implement
+        return false;
+    }
+
+    /** public bool Contains(Vector3 pos)
+     * Determine if the snake's body contains the given position.
+     *
+     * Parametrs:
+     *  pos - position to compare.
+     *
+     * Postconditions:
+     *  Contains will compare the given position with the entire body of
+     *  the snake.
+     */
+    public bool Contains(Vector3 pos) {
+        // TODO: Implement
+        return false;
+    }
+}
