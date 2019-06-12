e09443bcc3da2de4860ece929292266090c2e43d
Josie Thompson <josiahst@gmail.com>
2019-06-11 20:48:57 -0700

    ScoreBoard.cs has some documentation, but for now I'm going to neglect it to focus on getting the game working

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -4,33 +4,100 @@ using System;
 using System.IO;
 using UnityEngine;
 
-public class ScoreBoard {
+public class ScoreBoard : MonoBehaviour {
     string scorePath = "";
     List<string> names;
     List<int> scores;
+    string failureMessage = "";
 
     /** ScoreBoard()
      * Create a new ScoreBoard object
-     *
+     * 
+     * Reads from Scores.csv. If it can't read from Scores.csv, ToString will
+     * display an error message instead of high scores.
      */
     public ScoreBoard() {
-        scorePath = Path.Combine(Application.dataPath, "Scores.csv");
+        // Create the path to load from Scores.csv
+        try {
+            scorePath = Path.Combine(Application.dataPath, "Scores.csv");
+
+        // Temporarily disable warning: unused variable declaration
+        #pragma warning disable 0168
+        // In case Unity dataPath is null for some reason
+        } catch (ArgumentNullException e) {
+        #pragma warning restore 0168
+
+            failureMessage = "Could not find Scores.csv:\n" +
+                             "Unity data path is null!";
+
+        // Temporarily disable warning: unused variable declaration
+        #pragma warning disable 0168
+        // In case path contains ilegal characters for some reason
+        } catch (ArgumentException e) {
+        #pragma warning restore 0168
+
+            failureMessage = "Could not find Scores.csv:\n" +
+                             "Incompatible Unity data path:\n" +
+                             Application.dataPath;
+        }
+
         names = new List<string>();
         scores = new List<int>();
         LoadScores();
     }
 
+    /** public string ToString()
+     * Return the string representation of the scoreboard
+     *
+     * Has the format:
+     *
+     * High Scores
+     *
+     * name1: score1
+     * name2: score2
+     * ...
+     * nameN: scoreN
+     *
+     * where nameN and scoreN are names that users have inputed and their
+     * respective scores
+     *
+     * Exceptions:
+     *  ArgumentNullException - if names or scores is somehow null
+     */
     public override string ToString() {
+
+        // If ScoreBoard failed to read the scores, dsiplay error message
+        if (!String.IsNullOrEmpty(failureMessage)) {
+            return failureMessage;
+        }
+
         return names
-            .Zip(scores, (name, hiscore) => name+": "+hiscore)
+            // Put each name and respective score together
+            .Zip(scores, (name, score) => name+": "+score)
+            // Combine all the name-score lines together
             .Aggregate("High Scores\n\n", (str, line) => str+line+"\n")
             .TrimEnd('\n');
     }
 
+    /** public void UpdateScores(int i, string name, int score)
+     * Update the scoreboard with name and score.
+     *
+     * Parameters
+     *  i        - index of the score on the scoreboard
+     *  name     - name of the player with the high score
+     *  score    - respective score of the player
+     *
+     * Adds the name and the score at index i. If the length of the scores is
+     * greater than 10 after adding, the last score will be dropped.
+     *
+     * Assumes:
+     *  i is in [0, 9]
+     */
     public void UpdateScores(int i, string name, int score) {
         names.Insert(i, name);
         scores.Insert(i, score);
 
+        // Remove last score if scores has more than 10 scores
         int n = scores.Count;
         if (n > 10) {
             names.RemoveAt(n-1);
@@ -38,26 +105,63 @@ public class ScoreBoard {
         }
     }
 
+    /** public int ScoreIndex(int score)
+     * Give the index in the score list of this score
+     *
+     * Parameters
+     *  score - to compare with list of scores
+     *
+     * Returns
+     *  an integer from 0-9 correspodning to the index of the score list
+     *  the score should be placed in. If the score is not a high score,
+     *  returns 0.
+     */
     public int ScoreIndex(int score) {
-        return Enumerable.Range(0, 10)
-            .DefaultIfEmpty(-1)
-            .FirstOrDefault(i => i < scores.Count() && score > scores[i]);
+        // range of indices (from 1 to 10)
+        return Enumerable.Range(1, 10)
+            // grab the first index which has lower score than current
+            .FirstOrDefault(i => {
+                return (i-1) < scores.Count && score > scores[i-1];
+            }) - 1;
     }
 
+    /** void LoadScores()
+     * Load the scores from the csv file
+     *
+     * Loads the scores from Assets/Scores.csv and replaces whatever is stored
+     * in names and scores with the new scores.
+     *
+     * Side Effects:
+     *  names and scores are cleared if score file is successfully opened.
+     *  If not, or if scores contains a non-integer value, ToString will
+     *  display an error message.
+     */
     void LoadScores() {
+        // store file in a string so the file can be closed right away
         string scoreStr = "";
         try {
             // Open and read the scores file
             using (StreamReader scoreStream = new StreamReader(scorePath)) {
                 scoreStr = scoreStream.ReadToEnd();
-
             }
-        } catch (Exception e) {
-            Debug.Log("Something went wrong. "+e.ToString());
+        #pragma warning disable 0168
+        } catch (FileNotFoundException e) {
+        #pragma warning restore 0168
+
+            failureMessage = "Could not find Scores.csv in data path:\n" +
+                             Application.dataPath;
+            return;
+
+        #pragma warning disable 0168
+        } catch (DirectoryNotFoundException e) {
+        #pragma warning restore 0168
+
+            failureMessage = "Could not find data path:\n" +
+                             Application.dataPath;
             return;
         }
 
-        // Reset score arrays
+        // Reset names and scores
         names.Clear();
         scores.Clear();
 
@@ -78,7 +182,8 @@ public class ScoreBoard {
             // to scores array
             int nextScore;
             if (!Int32.TryParse(lineVals[1], out nextScore)) {
-                Debug.Log("Couldn't Parse Score for "+lineVals[0]);
+                failureMessage = "Could not parse score for " + lineVals[0] +
+                                 ": " + lineVals[1];
                 return;
             }
             scores.Add(nextScore);
@@ -92,7 +197,9 @@ public class ScoreBoard {
         try {
             // Combine the names and scores into a string
             string scoreStr = names
-                .Zip(scores, (name, hiscore) => name+", "+hiscore)
+                // Put each name and score together, delimited by comma
+                .Zip(scores, (name, score) => name+", "+score)
+                // Put all of the lines together into one string
                 .Aggregate((str, line) => str+line+"\n")
                 .TrimEnd('\n');
 
