f74ea9410d885b9873bcbcdcc3c9a9ff030e904b
Josie Thompson <dot.slash.josie@gmail.com>
2024-10-05 10:43:19 -0700

    Food is now spawned at start of game

--- Assets/Scripts/Snake/SnakeManager.cs
+++ Assets/Scripts/Snake/SnakeManager.cs
@@ -6,15 +6,24 @@ namespace Snake
     {
         /** The snake player object that will be spawned at the start of the game */
         [SerializeField] public GameObject snakePrefab;
-        
+
+        /** The food object prefab that the player should consume */
+        [SerializeField] public GameObject foodPrefab;
+
         /** The position to spawn the snake. TODO: use some sort of game object in-level */
         private readonly Vector3 _startPosition = Vector3.zero;
+
+        /** The dimensions of the game board. TODO: read from level settings */
+        private readonly RectInt _boardDimensions = new(-5, -5, 10, 10);
 
         /** A reference to the spawned snake */
         private GameObject _snake;
         
         /** A reference to the snake's body component */
         private SnakeBody _snakeBody;
+
+        /** A reference to the food object on the board */
+        private GameObject _food;
 
         public void Awake()
         {
@@ -26,6 +35,27 @@ namespace Snake
                                  "Adding default component now");
                 _snakeBody = _snake.AddComponent<SnakeBody>();
             }
+            _food = Instantiate(foodPrefab, RandomOpenSpace(), Quaternion.identity);
+        }
+
+        private Vector3 RandomOpenSpace()
+        {
+            var pos = RandomSpaceOnBoard();
+            while (_snakeBody.CollidesWith(pos))
+            {
+                pos = RandomSpaceOnBoard();
+            }
+            return pos;
+        }
+
+        private Vector3 RandomSpaceOnBoard()
+        {
+            var x = Random.Range(_boardDimensions.x,
+                                 _boardDimensions.x + _boardDimensions.width);
+
+            var y = Random.Range(_boardDimensions.y,
+                                 _boardDimensions.y + _boardDimensions.height);
+            return new Vector3(x, y, 0f);
         }
     }
 }
