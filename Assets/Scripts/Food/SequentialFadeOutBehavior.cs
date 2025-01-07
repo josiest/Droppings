198ab48e7e153c2f2122d72f8a0a93b31cf4f4c2
Josie Thompson <dot.slash.josie@gmail.com>
2025-01-07 15:51:35 -0800

    fixed bug with dropping fade animation out behavior

--- Assets/Scripts/Food/SequentialFadeOutBehavior.cs
+++ Assets/Scripts/Food/SequentialFadeOutBehavior.cs
@@ -3,31 +3,21 @@
 
 namespace Food
 {
-    enum FadeOutState
-    {
-        NotStarted,
-        FadingToWhite,
-        FadingOut
-    }
-
-    [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(Animator))]
     public class SequentialFadeOutBehavior : MonoBehaviour
     {
         public delegate void FadeOutEvent();
         public FadeOutEvent OnCompleted;
         public FadeOutEvent OnStagger;
 
-        public float FadeToWhiteTime = 0.1f;
-        public AnimationCurve FadeToWhiteCurve;
-        
         public float FadeOutTime = 0.2f;
-        public AnimationCurve FadeOutCurve;
 
         public float SequentialDelay = 0.05f;
 
         private void Awake()
         {
-            spriteRenderer = GetComponent<SpriteRenderer>();
+            anim = GetComponent<Animator>();
+            anim.speed = 1f / Mathf.Max(0.001f, FadeOutTime);
         }
 
         private void Update()
@@ -41,59 +31,21 @@ namespace Food
             {
                 staggerTime += Time.deltaTime;
             }
-
-            // Animation state machine
-            switch (currentState)
-            {
-                case FadeOutState.NotStarted:
-                default:
-                    break;
-
-                case FadeOutState.FadingToWhite:
-                    if (animationTime >= FadeToWhiteTime)
-                    {
-                        animationTime = 0f;
-                        currentState = FadeOutState.FadingOut;
-                    }
-                    else
-                    {
-                        float t = animationTime / FadeToWhiteTime;
-                        float alpha = FadeToWhiteCurve.Evaluate(t);
-                        spriteRenderer.color = Color.Lerp(originalColor, Color.white, alpha);
-                    }
-                    animationTime += Time.deltaTime;
-                    break;
-
-                case FadeOutState.FadingOut:
-                    if (animationTime >= FadeOutTime)
-                    {
-                        currentState = FadeOutState.NotStarted;
-                        animationTime = -1f;
-                        OnCompleted?.Invoke();
-                    }
-                    else
-                    {
-                        float t = animationTime / FadeOutTime;
-                        float alpha = FadeOutCurve.Evaluate(t);
-                        spriteRenderer.color = Color.Lerp(Color.white, Color.clear, alpha);
-                    }
-                    animationTime += Time.deltaTime;
-                    break;
-            }
         }
 
         public void FadeOut()
         {
-            animationTime = 0f;
             staggerTime = 0f;
-            currentState = FadeOutState.FadingToWhite;
+            anim.enabled = true;
         }
 
-        private FadeOutState currentState = FadeOutState.NotStarted;
+        public void CompleteAnimation()
+        {
+            OnCompleted?.Invoke();
+        }
+
         private SpriteRenderer spriteRenderer;
-        
-        private Color originalColor;
-        private float animationTime = -1f;
+        private Animator anim;
         private float staggerTime = -1f;
     }
 }
